b1d09cad564da26dd34a47fde3934aa393f33c8d
Lorin Thwaits <lorint@gmail.com>
2017-05-18 07:23:50 +0100

    Comprehensive testing now done by using an emulated terminal window

--- spec/fancy_gets_spec.rb
+++ spec/fancy_gets_spec.rb
@@ -4,27 +4,36 @@ include FancyGets
 describe FancyGets do
   before do
     @list = ["Skimboard", "Volleyball", "Kite", "Beach Ball", "Water Gun", "Frisbee"]
-    # Fake out a 80x10 window
-    allow(IO.console).to receive(:winsize).at_least(:once).and_return([10, 80])
   end
 
   it 'should have a version number' do
     expect(FancyGets::VERSION).to_not be nil
   end
 
-  describe "when using gets_list" do
-    describe "keyboard interaction with single select" do
+  describe "When using gets_list" do
+    describe "Keyboard interaction with single select" do
       it 'should allow the ENTER key to choose a list item' do
         # Just press ENTER
         expect(STDIN).to receive(:getch).and_return(13.chr)
-        expect(gets_list(@list)).to eq("Skimboard")
+        term = fake_terminal(80, 10) do
+          expect(gets_list(@list)).to eq("Skimboard")
+        end
+        expect(term[:lines][0][0..12]).to eq("> Skimboard <")
+        expect(term[:x]).to eq(0)
+        expect(term[:y]).to eq(6)
       end
 
       it 'should allow the down arrow to access a second item' do
         expect(STDIN).to receive(:getch).and_return(
           27.chr, 91.chr, 66.chr, # Down arrow
           13.chr)                 # Enter
-        expect(gets_list(@list)).to eq("Volleyball")
+        term = fake_terminal(80, 10) do
+          expect(gets_list(@list)).to eq("Volleyball")
+        end
+        expect(term[:lines][0][0..12]).to eq("  Skimboard  ")
+        expect(term[:lines][1][0..13]).to eq("> Volleyball <")
+        expect(term[:x]).to eq(0)
+        expect(term[:y]).to eq(6)
       end
 
       it 'should allow the down arrow and up arrow to be used' do
@@ -32,7 +41,13 @@ describe FancyGets do
           27.chr, 91.chr, 66.chr, # Down arrow
           27.chr, 91.chr, 65.chr, # Up arrow
           13.chr)                 # Enter
-        expect(gets_list(@list)).to eq("Skimboard")
+        term = fake_terminal(80, 10) do
+          expect(gets_list(@list)).to eq("Skimboard")
+        end
+        expect(term[:lines][0][0..12]).to eq("> Skimboard <")
+        expect(term[:lines][1][0..13]).to eq("  Volleyball  ")
+        expect(term[:x]).to eq(0)
+        expect(term[:y]).to eq(6)
       end
 
       it 'should not wrap around when the selection is already at the top and the up arrow is used' do
@@ -41,7 +56,12 @@ describe FancyGets do
           27.chr, 91.chr, 65.chr, # Up arrow
           27.chr, 91.chr, 65.chr, # Up arrow
           13.chr)                 # Enter
-        expect(gets_list(@list)).to eq("Skimboard")
+        term = fake_terminal(80, 10) do
+          expect(gets_list(@list)).to eq("Skimboard")
+        end
+        expect(term[:lines][0][0..12]).to eq("> Skimboard <")
+        expect(term[:x]).to eq(0)
+        expect(term[:y]).to eq(6)
       end
 
       it 'should not wrap around when the selection is already at the bottom and the down arrow is used' do
@@ -53,15 +73,50 @@ describe FancyGets do
           27.chr, 91.chr, 66.chr, # Down arrow
           27.chr, 91.chr, 66.chr, # Down arrow
           13.chr)                 # Enter
-        expect(gets_list(@list)).to eq("Frisbee")
+        term = fake_terminal(80, 10) do
+          expect(gets_list(@list)).to eq("Frisbee")
+        end
+        expect(term[:lines][5][0..10]).to eq("> Frisbee <")
+        expect(term[:x]).to eq(0)
+        expect(term[:y]).to eq(6)
+      end
+      it 'should accommodate shorter windows by having arrows to show continuation' do
+        expect(STDIN).to receive(:getch).and_return(
+          13.chr)                 # Enter
+        term = fake_terminal(80, 7) do
+          expect(gets_list(list: @list)).to eq("Skimboard")
+        end
+        expect(term[:lines].map{|line| line[0..12]}).to include("> Skimboard <")
+        # Not enough room to show the last one
+        expect(term[:lines].map{|line| line[0..10]}).to_not include("  Frisbee  ")
+        expect(term[:lines][3..-1].map{|line| line[0..13]}).to include("  ↓↓↓↓↓↓↓↓↓↓  ")
+
+        expect(STDIN).to receive(:getch).and_return(
+          27.chr, 91.chr, 66.chr, # Down arrow
+          27.chr, 91.chr, 66.chr, # Down arrow
+          27.chr, 91.chr, 66.chr, # Down arrow
+          27.chr, 91.chr, 66.chr, # Down arrow
+          13.chr)                 # Enter
+        term = fake_terminal(80, 7) do
+          expect(gets_list(list: @list)).to eq("Water Gun")
+        end
+        expect(term[:lines].map{|line| line[0..12]}).to include("> Water Gun <")
+        # We've already scrolled past the first one
+        expect(term[:lines].map{|line| line[0..12]}).to_not include("  Skimboard  ")
+        expect(term[:lines][0][0..13]).to eq("  ↑↑↑↑↑↑↑↑↑↑  ")
       end
     end
 
-    describe "keyboard interaction with multiple select" do
+    describe "Keyboard interaction with multiple select" do
       it 'should allow the SPACE BAR to choose a list item' do
         # Just press SPACE and ENTER
         expect(STDIN).to receive(:getch).and_return(" ", 13.chr)
-        expect(gets_list(@list, true)).to eq(["Skimboard"])
+        term = fake_terminal(80, 10) do
+          expect(gets_list(@list, true)).to eq(["Skimboard"])
+        end
+        expect(term[:lines][0][0..12]).to eq("> Skimboard <")
+        expect(term[:x]).to eq(0)
+        expect(term[:y]).to eq(6)
       end
 
       it 'should allow multiple items to be chosen' do
@@ -71,38 +126,91 @@ describe FancyGets do
           27.chr, 91.chr, 66.chr, # Down arrow
           " ",                    # Space
           13.chr)                 # Enter
-        expect(gets_list(@list, true)).to eq(["Volleyball", "Kite"])
+        term = fake_terminal(80, 10) do
+          expect(gets_list(@list, true)).to eq(["Volleyball", "Kite"])
+        end
+        expect(term[:lines][0][0..12]).to eq("  Skimboard  ")
+        expect(term[:lines][1][0..13]).to eq("> Volleyball <")
+        expect(term[:lines][2][0..7]).to eq("> Kite <")
+        expect(term[:x]).to eq(0)
+        expect(term[:y]).to eq(6)
       end
     end
   end
 
-  describe "when using gets_auto_suggest" do
-    it 'should pick a list item with just a few keystrokes' do
-      # Just press ENTER
+  describe "When using gets_password, it:" do
+    it 'should mask the typed characters with asterisks' do
       expect(STDIN).to receive(:getch).and_return(
-        "B",
-        "e",
-        13.chr)   # Enter
-      expect(gets_auto_suggest(@list)).to eq("Beach Ball")
+        "C", "o", "o", "l", "B", "e", "a", "n", "s", 13.chr)
+      term = fake_terminal(80, 10) do
+        expect(gets_password()).to eq("CoolBeans")
+      end
+      expect(term[:lines][0][0..10]).to eq("*********  ")
+    end
+
+    it 'should allow arrow keys to edit the text' do
+      expect(STDIN).to receive(:getch).and_return(
+        "a",
+        27.chr, 91.chr, 68.chr, # Left arrow
+        "C",
+        27.chr, 91.chr, 67.chr, # Right arrow
+        "t",
+        13.chr)                 # Enter
+      term = fake_terminal(80, 10) do
+        expect(gets_password()).to eq("Cat")
+      end
+      expect(term[:lines][0][0..5]).to eq("***   ")
+    end
+
+    it 'should allow backspace and delete keys to edit the text' do
+      expect(STDIN).to receive(:getch).and_return(
+        "X",
+        "P",
+        "a",
+        "X",
+        127.chr,                # Backspace
+        "s",
+        "s",
+        27.chr, 91.chr, 68.chr, # Left arrow
+        27.chr, 91.chr, 68.chr, # Left arrow
+        27.chr, 91.chr, 68.chr, # Left arrow
+        27.chr, 91.chr, 68.chr, # Left arrow
+        27.chr, 91.chr, 68.chr, # Left arrow
+        126.chr,                # Delete
+        13.chr)                 # Enter
+      term = fake_terminal(80, 10) do
+        expect(gets_password()).to eq("Pass")
+      end
+      expect(term[:lines][0][0..5]).to eq("****  ")
+    end
+  end
+
+  describe "When using gets_auto_suggest, it:" do
+    it 'should pick a list item with just a few keystrokes' do
+      expect(STDIN).to receive(:getch).and_return(
+        "B", "e", 13.chr)
+      term = fake_terminal(80, 10) do
+        expect(gets_auto_suggest(@list)).to eq("Beach Ball")
+      end
+      expect(term[:lines][0][0..14]).to eq("Be - Beach Ball")
     end
 
     it 'should be case-insensitive' do
       expect(STDIN).to receive(:getch).and_return(
-        "b",
-        "E",
-        "A",
-        13.chr)   # Enter
-      expect(gets_auto_suggest(@list)).to eq("Beach Ball")
+        "b", "E", "A", 13.chr)   # Enter
+      term = fake_terminal(80, 10) do
+        expect(gets_auto_suggest(@list)).to eq("Beach Ball")
+      end
+      expect(term[:lines][0][0..15]).to eq("bEA - Beach Ball")
     end
 
     it 'should just return the typed text when the first few letters match but not the whole thing' do
       expect(STDIN).to receive(:getch).and_return(
-        "B",
-        "e",
-        "a",
-        "X",
-        13.chr)   # Enter
-      expect(gets_auto_suggest(@list)).to eq("BeaX")
+        "B", "e", "a", "X", 13.chr)   # Enter
+      term = fake_terminal(80, 10) do
+        expect(gets_auto_suggest(@list)).to eq("BeaX")
+      end
+      expect(term[:lines][0][0..16]).to eq("BeaX -           ")
     end
 
     it 'should allow arrow keys to edit the text' do
@@ -113,7 +221,10 @@ describe FancyGets do
         27.chr, 91.chr, 67.chr, # Right arrow
         "a",
         13.chr)                 # Enter
-      expect(gets_auto_suggest(@list)).to eq("Beach Ball")
+      term = fake_terminal(80, 10) do
+        expect(gets_auto_suggest(@list)).to eq("Beach Ball")
+      end
+      expect(term[:lines][0][0..15]).to eq("Bea - Beach Ball")
     end
 
     it 'should allow backspace and delete keys to edit the text' do
@@ -132,7 +243,10 @@ describe FancyGets do
         27.chr, 91.chr, 68.chr, # Left arrow
         126.chr,                # Delete
         13.chr)                 # Enter
-      expect(gets_auto_suggest(@list)).to eq("Beach Ball")
+      term = fake_terminal(80, 10) do
+        expect(gets_auto_suggest(@list)).to eq("Beach Ball")
+      end
+      expect(term[:lines][0][0..16]).to eq("Beac - Beach Ball")
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,55 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'fancy_gets'
+
+# Listen to stdout, receiving arrow directions, text, newlines, and backspaces.
+# Return an array of what each line of the screen would hold.
+def fake_terminal(x_size, y_size)
+  begin
+    allow(IO.console).to receive(:winsize).at_least(:once).and_return([y_size, x_size])
+
+    original_stdout = $stdout
+    $stdout = StringIO.new
+    yield
+    lines = y_size.times.inject([]){|s, v| s << Array.new(x_size, ' ')}
+    x = 0
+    y = 0
+    idx = 0
+    while idx < $stdout.string.length
+      ch = $stdout.string[idx]
+      if ch.ord == 27 # Escape?
+        case $stdout.string[idx + 2].ord
+        when 65 # Up
+          y -= 1
+        when 66 # Down
+          y += 1
+        when 67 # Right
+          x += 1
+        when 68 # Left
+          x -= 1
+        end
+        idx += 3
+      elsif ch == "\b" # Backspace (which really acts mostly like a left arrow)
+        x -= 1 unless x == 0
+        idx += 1
+      elsif ch == "\n" # Newline
+        x = 0
+        y += 1
+        idx += 1
+      else
+        lines[y][x] = ch
+        x += 1
+        if x == x_size
+          y += 1
+          x = 0
+        end
+        idx += 1
+      end
+      if y > y_size
+        raise RuntimeError, "Went past vertical terminal space for #{x_size} x #{y_size} window.  (And scrolling is not implemented yet)"
+      end
+    end
+    {lines: lines.map(&:join), x: x, y: y, chars: $stdout.string}
+  ensure
+    $stdout = original_stdout
+  end
+end
